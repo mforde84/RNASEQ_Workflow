9c7797d68f1beee283ca4ee39d0219604fa35edf
Martin Forde <mforde84@gmail.com>
2018-08-30 12:46:55 -0500

    Delete gitupdate.sh
    unneeded for public

--- gitupdate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export COMMENT=$1;
-export AGENT=$(eval $(ssh-agent -s) | sed 's/^.*pid //g');
-ssh-add ~/.ssh/id_rsa;
-git remote set-url origin git@git.cri.uchicago.edu:mforde/RNASEQ_Workflow.git
-git add .;
-git commit -m "$COMMENT";
-git push origin master;
-kill -9 $AGENT;
